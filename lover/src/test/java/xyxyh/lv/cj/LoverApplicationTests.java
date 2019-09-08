d3743690ecc944a24c68af389cf95c712f555d5d
1160300702 <2648361987@qq.com>
2019-09-08 15:19:25 +0800

    测试

---
+++ lover/src/test/java/xyxyh/lv/cj/LoverApplicationTests.java
@@ -0,0 +1,16 @@
+package xyxyh.lv.cj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
